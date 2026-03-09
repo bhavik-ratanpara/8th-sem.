28ec300077e643669053fc17202b7f5bf883de34
Bhavik Ratanpara <bhavikratanpara5@gmail.com>
2026-03-09 17:52:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3872024424-a1e04",
+  "appId": "1:400286265436:web:32ea330a78780a621af699",
+  "apiKey": "AIzaSyAruBD-0ZmjScZkoTnfKY_sMnXPFk55n1g",
+  "authDomain": "studio-3872024424-a1e04.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "400286265436"
+};
