555ef0b03b25e98286733edb189c439ca7831245
Bhavik Ratanpara <bhavikratanpara5@gmail.com>
2026-03-20 12:45:37 +0000

    still see ,for mobile view its overlaping,,so increase the gap between s

--- src/components/typewriter-hero.tsx
+++ src/components/typewriter-hero.tsx
@@ -135,7 +135,7 @@ export function TypewriterHero() {
                 display: 'flex',
                 flexDirection: 'row',
                 gap: '8px',
-                marginTop: '56px',
+                marginTop: '92px',
                 flexWrap: 'nowrap',
                 alignItems: 'center',
               }}
